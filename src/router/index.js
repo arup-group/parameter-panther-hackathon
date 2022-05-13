865a14ac6a3080331c68db238350ae673cbb748d
dtnaughton <77862637+dtnaughton@users.noreply.github.com>
2022-05-13 14:43:17 -0400

    update index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import store from "@/store";
+import store from "@/store";
 import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
@@ -27,39 +27,39 @@ const router = new VueRouter({
   routes
 });
 
-// router.beforeEach(async (to, from, next) => {
-//   console.log("working?", to)
-//   if (to.query.access_code) {
-//     // If the route contains an access code, exchange it
-//     try {
-//       await store.dispatch("exchangeAccessCode", to.query.access_code);
-//     } catch (err) {
-//       console.warn("exchange failed", err);
-//     }
-//     // Whatever happens, go home.
-//     next("/");
-//   } else {
-//     if (to.name !== "Login") {
-//       // if the user is going to a page that isn't the login page, check that they're logged in
-//       try {
-//         console.log("in here")
-//         const res = await store.dispatch("getUser");
-//         console.log("res:", res);
-//         next();
-//         console.log("got down here")
-//       } catch (err) {
-//         console.log("redirecting");
-//         // redirect to login page if the user is not signed in
-//         next("/login");
-//         return;
-//         // console.log("here? for some reason")
-//         // if (err.message === "NOT_SIGNED_IN") next("/login");
-//         // else next("/");
-//       }
-//     }
-//   }
-//   console.log("got here");
-//   next();
-// });
+router.beforeEach(async (to, from, next) => {
+  console.log("working?", to)
+  if (to.query.access_code) {
+    // If the route contains an access code, exchange it
+    try {
+      await store.dispatch("exchangeAccessCode", to.query.access_code);
+    } catch (err) {
+      console.warn("exchange failed", err);
+    }
+    // Whatever happens, go home.
+    next("/");
+  } else {
+    if (to.name !== "Login") {
+      // if the user is going to a page that isn't the login page, check that they're logged in
+      try {
+        console.log("in here")
+        const res = await store.dispatch("getUser");
+        console.log("res:", res);
+        next();
+        console.log("got down here")
+      } catch (err) {
+        console.log("redirecting");
+        // redirect to login page if the user is not signed in
+        next("/login");
+        return;
+        // console.log("here? for some reason")
+        // if (err.message === "NOT_SIGNED_IN") next("/login");
+        // else next("/");
+      }
+    }
+  }
+  console.log("got here");
+  next();
+});
 
 export default router
